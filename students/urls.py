9f9db112c36ca19b817fcd3d6a191af58296088a
shyamanurag <ranchissi@gmail.com>
2025-06-27 01:16:18 +0530

    Fix URL routing conflicts and add missing views - Students URL conflict resolved, missing views added, templates created

--- students/urls.py
+++ students/urls.py
@@ -4,9 +4,16 @@ from . import views
 from .api import StudentViewSet
 
 router = DefaultRouter()
-router.register(r'api/students', StudentViewSet)
+router.register(r'students', StudentViewSet)
 
 urlpatterns = [
+    # Web interface URLs
     path('', views.StudentListView.as_view(), name='student-list'),
-    path('', include(router.urls)),
+    path('add/', views.StudentCreateView.as_view(), name='student-add'),
+    path('<int:pk>/edit/', views.StudentUpdateView.as_view(), name='student-edit'),
+    path('<int:pk>/delete/', views.StudentDeleteView.as_view(), name='student-delete'),
+    path('<int:pk>/', views.StudentDetailView.as_view(), name='student-detail'),
+    
+    # API URLs - moved to separate path to avoid conflicts
+    path('api/', include(router.urls)),
 ]
--- students/views.py
+++ students/views.py
@@ -1,10 +1,87 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.contrib import messages
 from .models import Student
 
 class StudentListView(ListView):
     model = Student
     template_name = 'students/student_list.html'
-    context_object_name = 'studentlist'
+    context_object_name = 'students'
+    paginate_by = 50
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'Students'
+        context['total_students'] = Student.objects.count()
+        return context
+
+class StudentDetailView(DetailView):
+    model = Student
+    template_name = 'students/student_detail.html'
+    context_object_name = 'student'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = f'Student - {self.object.first_name} {self.object.last_name}'
+        return context
+
+class StudentCreateView(CreateView):
+    model = Student
+    template_name = 'students/student_form.html'
+    fields = [
+        'admission_number', 'roll_number', 'first_name', 'last_name', 
+        'date_of_birth', 'gender', 'phone', 'email', 'address',
+        'class_enrolled', 'section_enrolled', 'admission_date',
+        'parent_name', 'parent_phone', 'parent_email', 'emergency_contact',
+        'photo', 'is_active'
+    ]
+    success_url = reverse_lazy('student-list')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'Add New Student'
+        context['form_title'] = 'Add New Student'
+        return context
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Student added successfully!')
+        return super().form_valid(form)
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    template_name = 'students/student_form.html'
+    fields = [
+        'admission_number', 'roll_number', 'first_name', 'last_name', 
+        'date_of_birth', 'gender', 'phone', 'email', 'address',
+        'class_enrolled', 'section_enrolled', 'admission_date',
+        'parent_name', 'parent_phone', 'parent_email', 'emergency_contact',
+        'photo', 'is_active'
+    ]
+    success_url = reverse_lazy('student-list')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = f'Edit Student - {self.object.first_name} {self.object.last_name}'
+        context['form_title'] = f'Edit Student - {self.object.first_name} {self.object.last_name}'
+        return context
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Student updated successfully!')
+        return super().form_valid(form)
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    template_name = 'students/student_confirm_delete.html'
+    success_url = reverse_lazy('student-list')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = f'Delete Student - {self.object.first_name} {self.object.last_name}'
+        return context
+    
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, 'Student deleted successfully!')
+        return super().delete(request, *args, **kwargs)
 
 # Create your views here.
